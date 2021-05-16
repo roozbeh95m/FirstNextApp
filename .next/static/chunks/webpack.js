27b97b6a3088c0d66e667ea97f141acbfcbd16b3
roozbeh mohammadzadeh <roozbeh1995m@gmail.com>
2021-05-16 08:26:08 +0430

    fixError

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2465f2783e14fbc72c31"; }
+/******/ 		__webpack_require__.h = function() { return "58007088c3329652f836"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
